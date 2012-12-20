e746b2ac6d8f5d371f97044e36f94d9d4a671f16
kphuanghk <kphuanghk@gmail.com>
2012-12-20 22:36:54 +0800

    Update .hkgs.el

--- .hkgs.el
+++ .hkgs.el
@@ -9,15 +9,20 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Define Variables
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (defvar R_APP_ROOT "")
+;; (defvar R_VIEWS "")
+;; (defvar R_CONTROLLERS "")
+;; (defvar R_MODELS "")
+
 
 (defun hkgs-init (rails_app_path)
 "Initialize Rails app, views, models and controllers path.
 RAILS_APP_PATH is the root path of the application folder.
-Do not place "/" at the end of the path"
+"
   (progn (setq R_APP_ROOT rails_app_path)
-   (setq R_VIEWS (concat R_APP_ROOT "/views"))
-	 (setq R_CONTROLLERS (concat R_APP_ROOT "/controllers"))
-	 (setq R_MODELS (concat R_APP_ROOT "/models"))))
+	 (setq R_VIEWS (concat R_APP_ROOT "/app/views"))
+	 (setq R_CONTROLLERS (concat R_APP_ROOT "/app/controllers"))
+	 (setq R_MODELS (concat R_APP_ROOT "/app/models"))))
 
 ;; Unit Testing
 ;; (hkgs-init "C:/RailsInstaller/Learning/demo/app")
@@ -46,7 +51,25 @@ Mode 2: List controller#view,
     (setq comp-view-selected 
 	  (completing-read "View to Edit: " 
 			   comp-view-options))
-    (message comp-view-options)))
+    (hkgs-get-view-file comp-view-selected)))
+
+
+(defun hkgs-find-view-mode2 ()
+"Find the view of Rails web component. In the future, it will support
+two modes of finding Rails view:
+Mode 1: Select controller name, then select its views.
+  e.g. Step 1: home   Step 2: index create destory edit
+Mode 2: List controller#view, 
+  e.g. home#index home#create home#destory home#edit
+" 
+  (interactive)
+  (let (comps comp-view-selected)
+    (setq comps (directory-files R_VIEWS nil "^[^\.]"))
+    (setq comp-view-options (pack-comp-list comps))
+    (setq comp-view-selected 
+	  (completing-read "Find View: " comp-view-options))
+    (hkgs-get-view-file comp-view-selected)))
+
 
 ;; Pack view here
 (defun pack-view (ctrl view-names)
@@ -61,9 +84,35 @@ Mode 2: List controller#view,
 ;; Unit Testing
 ;; (pack-view "home" '("index" "create" "edit" "show"))
 
+
+(defun pack-comp-list (comps)
+  " Pack Rails components and return list contains comp#file."
+  (let (result)
+    (dolist (comp comps result)
+      (setq result 
+	    (append result 
+		    (pack-view comp 
+			       (directory-files
+				(concat R_VIEWS "/" comp) nil "erb$")))))
+    result))
+
+;; Unit Testing
+;; (pack-comp-list '("home" "events"))
+
 (defun hkgs-view-prefix (value)
   (substring value 0 (string-match "\\." value)))
 ;; Unit Testing
 ;; (hkgs-view-prefix "abc.html.erb")
 
+;; retrive view file
+;; input as controller#view format
+(defun hkgs-get-view-file (view)
+  (let (view-fullpath)
+    (setq view (split-string view "#"))
+    (setq view-fullpath 
+	  (concat R_VIEWS "/" (car view) "/" (car (cdr view)) ".html.erb"))
+    (find-file view-fullpath)))
 
+;; Key bindings for HKGS Rails Plugin
+(global-set-key (kbd "C-c C-c") 'hkgs-find-controller)
+(global-set-key (kbd "C-c C-v") 'hkgs-find-view-mode2)
